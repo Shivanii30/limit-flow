a998970818768ac26135b6972aae8b5f9159db4a
Shivani Kshirsagar <108947015+Shivanii30@users.noreply.github.com>
2026-04-08 11:53:36 +0530

    Rename package from 'limit-flow' to 'limitflow'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="limit-flow",
+    name="limitflow",
     version="0.1.0",
     description="Adaptive rate limiting middleware for Python APIs",
     author="Shivani Kshirsagar",
