0e437159ca3cc879c86ac1aaf78ffb595e3c46a4
Shivani Kshirsagar <108947015+Shivanii30@users.noreply.github.com>
2026-04-08 12:01:29 +0530

    Delete limit-flow directory

--- limit-flow/algorithms/fixed_window.py
+++
@@ -1,9 +0,0 @@
-class FixedWindow:
-    def __init__(self, limit, window, store):
-        self.limit = limit
-        self.window = window
-        self.store = store
-
-    def is_allowed(self, key):
-        count = self.store.increment(key, self.window)
-        return count <= self.limit
